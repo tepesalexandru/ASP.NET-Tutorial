ed593a372ab4eb37c8d79ea0ffa79349817a6e9d
Tepes Alexandru <godzplayer@hotmail.com>
2020-11-10 10:49:18 +0200

    Add project files.

---
+++ TutorialApp/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TutorialApp.Models;
+using TutorialApp.Services;
+
+namespace TutorialApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        
+        // [FromBody]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ TutorialApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TutorialApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        [JsonPropertyName("desc")]
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+    }
+}
